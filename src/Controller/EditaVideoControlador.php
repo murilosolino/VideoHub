dead48631795dde9e39058c316f679596c072287
murilosolino <murilo.solino@outlook.com>
2025-12-07 09:02:15 -0300

    chore: filter_inpute validate in controllers

--- src/Controller/EditaVideoControlador.php
+++ src/Controller/EditaVideoControlador.php
@@ -6,6 +6,7 @@ namespace Aluraplay\Mvc\Controller;
 
 use Aluraplay\Mvc\Entity\Video;
 use Aluraplay\Mvc\Repository\RespositorioVideos;
+use Exception;
 
 class EditaVideoControlador implements Controller
 {
@@ -16,8 +17,21 @@ class EditaVideoControlador implements Controller
     {
 
         try {
-            $video = new Video($_POST['url'], $_POST['titulo']);
-            $video->setId(intval($_GET['id']));
+
+            $url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+            $titulo = filter_input(INPUT_POST, 'titulo');
+            $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+            if (
+                $url === false || $url === null || !preg_match('/^https?:\/\/[^\s]+$/', $url) ||
+                empty(trim($titulo)) ||
+                $id === false || $id === null
+            ) {
+                throw new Exception("Dados inválidos");
+            }
+
+            $video = new Video($url, $titulo);
+            $video->setId(intval($id));
 
             if (array_key_exists("image", $_FILES) && $_FILES["image"]["error"] === UPLOAD_ERR_OK) {
                 $uploadPath = __DIR__ . "/../../public/img/uploads";
@@ -27,7 +41,9 @@ class EditaVideoControlador implements Controller
 
             $result = $this->respositorioVideos->atualizar($video);
 
-            $result ? header('Location: /?success=1') : throw new \Exception('Ocorreu um erro durante a atualização do registro no banco de dados');
+            $result ? header('Location: /?success=1') : throw new Exception(
+                'Ocorreu um erro durante a atualização do registro no banco de dados'
+            );
         } catch (\Throwable $th) {
             header('Location: /?success=0');
             exit();
--- src/Controller/NovoVideoControlador.php
+++ src/Controller/NovoVideoControlador.php
@@ -6,6 +6,7 @@ namespace Aluraplay\Mvc\Controller;
 
 use Aluraplay\Mvc\Entity\Video;
 use Aluraplay\Mvc\Repository\RespositorioVideos;
+use Exception;
 
 class NovoVideoControlador implements Controller
 {
@@ -17,7 +18,17 @@ class NovoVideoControlador implements Controller
 
         try {
 
-            $video = new Video($_POST['url'], $_POST['titulo']);
+            $url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+            $titulo = filter_input(INPUT_POST, 'titulo');
+
+            $video = new Video($url, $titulo);
+
+            if (
+                $url === false || $url === null || !preg_match('/^https?:\/\/[^\s]+$/', $url) ||
+                empty(trim($titulo))
+            ) {
+                throw new Exception("Dados inválidos");
+            }
 
             if (array_key_exists("image", $_FILES) && $_FILES["image"]["error"] === UPLOAD_ERR_OK) {
                 $fileName = basename($_FILES['image']['name']);
@@ -28,7 +39,7 @@ class NovoVideoControlador implements Controller
 
             $result = $this->respositorioVideos->inserir($video);
 
-            $result ? header('Location: /?success=1') : throw new \Exception('Ocorreu um erro durante a inserção no banco de dados');
+            $result ? header('Location: /?success=1') : throw new Exception('Ocorreu um erro durante a inserção no banco de dados');
         } catch (\Throwable $th) {
             header('Location: /?success=0');
             exit();
